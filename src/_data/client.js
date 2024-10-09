08a1b7a11558ebcc41b9772834345171665caa49
Sam Robinson <samrobinson@Sams-Mac-mini.home>
2024-10-09 05:48:37 -0400

    Og image x2

--- src/_data/client.js
+++ src/_data/client.js
@@ -17,5 +17,5 @@ module.exports = {
         "instagram": "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://peak-pressurewash.com"
 };
