57bf7052def1532f705f8ae2f514272ad2389377
Sam Robinson <samrobinson@Sams-Mac-mini.home>
2024-12-30 17:37:20 -0500

    Initial commit for Northbound Consulting

---
+++ src/_data/client.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "name": "Peak Pressure Wash",
+    "email": "peakpressurewash@outlook.com",
+    "phoneForTel": "901-652-3925",
+    "phoneFormatted": "(901) 652-3925",
+    "address": {
+        "lineOne": "Senatobia",
+        "lineTwo": "Second Address Line",
+        "city": "Senatobia",
+        "state": "MS",
+        "zip": "38668",
+        "country": "Mid-South, USA",
+        "mapLink": "https://maps.app.goo.gl/YmrEkD8Lcor9HFwu9"
+    },
+    "socials": {
+        "facebook": "https://www.facebook.com/",
+        "instagram": "https://www.instagram.com/",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://peak-pressurewash.com"
+};
